0ed6f3e60628aac45c980ba30282f5d6c3c28ccf
Santiagomrn <santiagomrn98@gmail.com>
2019-11-24 21:03:46 -0600

    :(

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdint.h> 
 #include "tm4c1294ncpdt.h"
 
-#define RST 	0x10
-#define CS 		0x08
-#define RS 		0x04
-#define WR 		0x02
-#define RD 		0x01
+#define RST 	0x10 //RS
+#define CS 		0x08 //
+#define RS 		0x04 //D/CX
+#define WR 		0x02 //WR
+#define RD 		0x01 //RDX
 
 
 
@@ -37,34 +37,36 @@ void delay(uint32_t tiempo){
 
 void write_parameter(uint8_t param){
 	GPIO_PORTD_DATA_R |= RS;
-	GPIO_PORTD_DATA_R &= ~CS;
+	GPIO_PORTD_DATA_R |= RD;
+	GPIO_PORTD_DATA_R &= ~WR;
 	GPIO_PORTA_DATA_R = param;
-	GPIO_PORTD_DATA_R |= CS;
+	GPIO_PORTD_DATA_R |= WR;
 }
 
 void send_cmd(uint8_t cmd){
 	GPIO_PORTD_DATA_R &= ~RS;
-	GPIO_PORTD_DATA_R &= ~CS;
-	
+	GPIO_PORTD_DATA_R |= RD;
+	GPIO_PORTD_DATA_R &= ~WR;
 	GPIO_PORTA_DATA_R = cmd;
-	
-	GPIO_PORTD_DATA_R |= CS;
+	GPIO_PORTD_DATA_R |= WR;
 }
 
 void init(void){
 	init_portD();
 	init_portA();
 	//Se genera un flanco de subida
+	GPIO_PORTD_DATA_R |= CS;
+	GPIO_PORTD_DATA_R |= RS;
+	GPIO_PORTD_DATA_R |= RD;
 	GPIO_PORTD_DATA_R |= RST; //Se pone en uno 
-	delay(1000);
-	GPIO_PORTD_DATA_R &= ~RST; //Se baja
-	delay(10000);
-	GPIO_PORTD_DATA_R |= RST; //Se vuelve a subir 
-	delay(120000);
 	
 	send_cmd(0x01); //Se envia el comando de Software Reset
-	delay(50*16); //Se espera un tiempo 
+	delay(16000000/190); //Se espera un tiempo 
+	
+	send_cmd(0x29);// Display ON
 	send_cmd(0x28);// Se apaga la pantalla con Display OFF
+	
+	send_cmd(0x21); //Display Inversion ON
 	
 	send_cmd(0xc0); //Se realiza un Power Control 1
 	write_parameter(0x26);//Se configura a 4.75  volts 
@@ -84,6 +86,7 @@ void init(void){
 	
 	send_cmd(0x3A);// Pixel Format Set
 	write_parameter(0x55);	//Se define el formato de 16 bits 
+	
 	
 	send_cmd(0xB1);//Frame Rate Control
 	write_parameter(0x00);	//Toma el fosc para la division
@@ -136,56 +139,31 @@ void init(void){
 	write_parameter(0x36);
 	write_parameter(0x0F);
 	
-	send_cmd(0x11); //Sleep Out
-	delay(120000);
+	delay(2*16000000);
 	
-	//send_cmd(0x21); //Display Inversion ON
 	
+
 	send_cmd(0x29); //Display ON
-	
-	send_cmd(0x2C); //Memory Write Inicio de escritura de datos 
+	send_cmd(0x21); //Display Inversion ON
+
 }
 
 
-
-
-void Escribiendo_datos(uint16_t datos){
-GPIO_PORTD_DATA_R |= 0x04;																		 	  // [Reset=0 CSX=0 D/CX=1 WRX=0 RDX=0]
-delay(0x3E80);                       														// Realizamos un retraso de 1us 
-GPIO_PORTD_DATA_R &= ~0x08;																		    // [Reset=1 CSX=0 D/CX=1 WRX=1 RDX=1]
-delay(0x3E80);                       														// Realizamos un retraso de 1us 
-GPIO_PORTA_DATA_R = datos;													          	  // Mandamos datos 
-delay(0x3E80);                       														// Realizamos un retraso de 1us 
-GPIO_PORTD_DATA_R |= 0x08;																			  // [Reset=0 CSX=1 D/CX=0 WRX=0 RDX=0]
-}
-
-void Escribiendo_comandos(uint16_t comando){	
-GPIO_PORTD_DATA_R &= ~0x04;																		 	  // [Reset=1 CSX=1 D/CX=0 WRX=1 RDX=1]
-delay(0x3E80);                       														// Realizamos un retraso de 1us 
-GPIO_PORTD_DATA_R &= ~0x08;																		    // [Reset=1 CSX=0 D/CX=1 WRX=1 RDX=1]
-delay(0x3E80);                       														// Realizamos un retraso de 1us 
-GPIO_PORTA_DATA_R = comando;												          	  // Mandamos comando 
-delay(0x3E80);                       														// Realizamos un retraso de 1us 
-GPIO_PORTD_DATA_R |= 0x08;																			  // [Reset=0 CSX=1 D/CX=0 WRX=0 RDX=0]
-}
-// Variables globales
-uint16_t CursorX;
-uint16_t CursorY;
 
 void Direccion_de_la_Pagina(uint16_t Inicio, uint16_t  Final){
-	Escribiendo_comandos(0x2B);
-	Escribiendo_datos(0x00);
-	Escribiendo_datos(Inicio);
-	Escribiendo_datos(0x00);
-	Escribiendo_datos(Final);	
+	send_cmd(0x2B);
+	write_parameter(0x00);
+	write_parameter(Inicio);
+	write_parameter(0x00);
+	write_parameter(Final);	
 }
 
 void Columna_del_Cuadro(uint16_t Inicio, uint16_t  Final){
-	Escribiendo_comandos(0x2A);
-	Escribiendo_datos(0x00);
-	Escribiendo_datos(Inicio);
-	Escribiendo_datos(0x00);
-	Escribiendo_datos(Final);	
+	send_cmd(0x2A);
+	write_parameter(0x00);
+	write_parameter(Inicio);
+	write_parameter(0x00);
+	write_parameter(Final);	
 }
 
 void Fondo_de_Pantalla(void){
@@ -193,17 +171,13 @@ Columna_del_Cuadro(0,239);													// Limitamos el Cuadro en columnas
 Direccion_de_la_Pagina(0,319);										// Limitamos el pagina
 send_cmd(0x2C);													// Habilitamos escribir en memoria. Realiza la transferencia de datos de MCU a la trama de memoria.
 	
-GPIO_PORTD_DATA_R |= RS;													// [Reset=0 CSX=0 D/CX=1 WRX=0 RDX=0]
-delay(0x3E80);                       							// Realizamos un retraso de 1us 
-GPIO_PORTD_DATA_R &= ~0x08;											  	// [Reset=1 CSX=0 D/CX=1 WRX=1 RDX=1]
-	
 	for(int i=0; i<38400; i++){												// Realizamos recorrido de toda la pantalla
 	  write_parameter(0x00);												// Determinamos color de 1/4 de pantalla
 		write_parameter(0xAA);												// Determinamos color de 1/4 de pantalla
 		write_parameter(0xAA);												// Determinamos color de 1/4 de pantalla
 		write_parameter(0x00);												// Determinamos color de 1/4 de pantalla
 	}	
-GPIO_PORTD_DATA_R |= 0x08;								  			  // [Reset=0 CSX=1 D/CX=0 WRX=0 RDX=0]	
+
 }
 
 int main(void){
